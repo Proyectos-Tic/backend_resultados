375e4544aa5c5d1cd745bc63c51728838065450a
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-11-01 18:08:47 -0500

    Endpoints using the controller methods

---
+++ blueprints/candidatos_blueprint.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request
+
+from controllers.candidatos_controller import CandidatosController
+
+candidatos_blueprint = Blueprint('candidatos_blueprint', __name__)
+candidatos_controller = CandidatosController()
+
+@candidatos_blueprint.route("/candidato/all", methods=["GET"])
+def get_all_candidates():
+    response = candidatos_controller.index()
+    return response, 200
+
+@candidatos_blueprint.route("/candidato/<string:id>", methods=["GET"])
+def get_candidate_by_id(id):
+    response = candidatos_controller.show(id)
+    return response, 200
+
+@candidatos_blueprint.route("/candidato/create", methods=["POST"])
+def create_candidate():
+    candidate = request.get_json()
+    response = candidatos_controller.create(candidate)
+    return response, 201
+
+@candidatos_blueprint.route("/candidato/update/<string:id>", methods=["PATCH"])
+def update_candidate(id):
+    candidate = request.get_json()
+    response = candidatos_controller.update(id, candidate)
+    return response, 201
+
+@candidatos_blueprint.route("/candidato/delete/<string:id>", methods=["DELETE"])
+def delete_candidate(id):
+    response = candidatos_controller.delete(id)
+    return response, 204
---
+++ blueprints/mesas_blueprint.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request
+
+from controllers.mesas_controller import MesasController
+
+mesas_blueprint = Blueprint('mesas_blueprint', __name__)
+mesas_controller = MesasController()
+
+@mesas_blueprint.route("/mesa/all", methods=["GET"])
+def get_all_tables():
+    response = mesas_controller.index()
+    return response, 200
+
+@mesas_blueprint.route("/mesa/<int:id>", methods=["GET"])
+def get_table_by_id(id):
+    response = mesas_controller.show(id)
+    return response, 200
+
+@mesas_blueprint.route("/mesa/create", methods=["POST"])
+def create_table():
+    table = request.get_json()
+    response = mesas_controller.create(table)
+    return response, 201
+
+@mesas_blueprint.route("/mesa/update/<int:id>", methods=["PATCH"])
+def update_table(id):
+    table = request.get_json()
+    response = mesas_controller.update(id, table)
+    return response, 201
+
+@mesas_blueprint.route("/mesa/delete/<int:id>", methods=["DELETE"])
+def delete_table(id):
+    response = mesas_controller.delete(id)
+    return response, 204
