32ecc7cadf4d376b0af47e0d90af599378f34016
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-11-03 18:30:53 -0500

    First generic CRUD methods

--- repositories/interfaceRepository.py
+++ repositories/interfaceRepository.py
@@ -1,4 +1,5 @@
 import json
+from bson import ObjectId
 import pymongo
 import certifi
 from typing import Generic, TypeVar, get_args
@@ -29,13 +30,41 @@ class InterfaceRepository(Generic[T]):
 
 #==========SPRINT 1 ==========
     def find_all(self) -> list:
-        pass
+        dataset = []
+        for document in self.current_collection.find():
+            document['_id'] = document['_id'].__str__()
+            document = self.transform_object_ids(document)
+            document = self.get_values_db_ref(document)
+            dataset.append(document)
+        return dataset
 
-    def find_by_id(self, id: str) -> T:
-        pass
+    def find_by_id(self, id: str) -> dict:
+        _id = ObjectId(id)
+        # if document not found, document = None
+        document = self.current_collection.find_one({'_id': _id})
+        document = self.get_values_db_ref(document)
+        if document:
+            document['_id'] = document['_id'].__str__()
+        else:
+            # document not found
+            document = {}
+        return document
 
     def save(self, item: T) -> T:
-        pass
+        item = self.transform_refs(item)
+        if hasattr(item, '_id') and item._id != "":
+            # Update
+            # item without _id
+            delattr(item, '_id')
+            item = item.__dict__
+            # _id filter
+            id = item._id
+            _id = ObjectId(id)
+            updated_item = {'$set': item}
+            self.current_collection.update_one({'_id': _id}, updated_item)
+        else:
+            # Insert
+            _id = self.current_collection.insert_one(item.__dict__)
 
     # TODO: Verify the return type
     def update(self, id: str, item: T) -> dict:
@@ -48,7 +77,7 @@ class InterfaceRepository(Generic[T]):
     # TODO: Implement the query/format methods in just one method
     def get_values_db_ref(self, document) -> T:
         """
-        This method return a value compatible with Python
+        This method transform sub-documents
         Mongo -> Py
 
         :param document: Obj from MongoDB
@@ -64,6 +93,7 @@ class InterfaceRepository(Generic[T]):
 
     def transform_object_ids(self):
         """
+        This method transform sub-documents
         Mongo -> Py
         """
         pass
