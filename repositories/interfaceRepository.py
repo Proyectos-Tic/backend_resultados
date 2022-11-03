f257abf7de285f8af0f5d6f9454f800ab3482a50
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-11-03 17:32:53 -0500

    first interface changes

--- repositories/interfaceRepository.py
+++ repositories/interfaceRepository.py
@@ -1,15 +1,26 @@
 import json
 import pymongo
 import certifi
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, get_args
 
 T = TypeVar('T')
+
+print(get_args(list[int])[0].__name__)
 
 class InterfaceRepository(Generic[T]):
     def __init__(self) -> None:
         ca = certifi.where()
         data_config = self.load_config()
-        client = pymongo.MongoClient(data_config.get('db-connection'), tlsCAFile=ca)
+        client = pymongo.MongoClient(
+            data_config.get('db-connection'), 
+            tlsCAFile=ca)
+
+        self.data_base = client.get_database(data_config.get('db-name'))
+        # Class name of the generic type: <class 'Mesas'>
+        model_class = get_args(self.__orig_bases__[0])
+        # Use the class name to get the collection: Mesas(model) -> mesas(collection)
+        self.collection = model_class[0].__name__.lower()
+        self.current_collection = self.data_base.get_collection(self.collection)
 
     def load_config(self):
         with open("config.json", "r") as config_file:
